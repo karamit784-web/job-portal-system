76271b2d1dbdb98bb76e179508c8c115cb7f3c86
karamit784-web <karamit784@gmail.com>
2026-08-04 16:20:04 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 app.use(express.json());
 
 
